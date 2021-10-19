ab6d7674b0a95ee2699e96229fdcf770dc25ff5e
duanhongyi <duanhongyi@doopai.com>
2021-10-19 15:47:11 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby Quick Start Guide
+
+This guide will walk you through deploying a Ruby application on [Drycc Workflow][https://github.com/drycc/workflow].
---
+++ web.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+set :port, ENV["PORT"] || 5000
+
+get '/healthz' do
+  status 200
+end
+
+get '/' do
+  whom = ENV["POWERED_BY"] || "Drycc"
+  revision = ENV['WORKFLOW_RELEASE'] || "unknown"
+  container = `hostname`.strip || "unknown"
+  "Powered by #{whom}\nRelease #{revision} on #{container}\n"
+end
